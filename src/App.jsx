abcfc8671e0dc61a4e6c1d7c511505370998f86e
Geoffrey Anderson <anderson.geoffrey@gmail.com>
2024-11-21 20:19:05 -0800

    bug fix to cycle from end. font size bigger for item text

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,17 @@ import './App.css'
 function App() {
   const [card, setCard] = useState(0)
   const handleCardClick = (index) => {
-    const next = index > data.length - 1 ? 0 : index;
+    const next = index > data.items.length - 1 ? 0 : index;
     setCard(next);
   }
   return (
     <>
       <div className="container">
-        <Nav data={data} card={card} clickHandler={handleCardClick} />
-        {data.map((item) => (
+        <div className="header">
+        <h1 className="title">{data.title}</h1>
+        <Nav data={data.items} card={card} clickHandler={handleCardClick} />
+        </div>
+        {data.items.map((item) => (
           <YarnCard
             className={card === item.id ? "selected" : ""}
             key={item.id}
