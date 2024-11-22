6e57cd9339a90cfd812f14d37e22bd3beec826a8
Geoffrey Anderson <anderson.geoffrey@gmail.com>
2024-11-21 21:15:12 -0800

    counter implemented

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,33 @@
 import PropTypes from 'prop-types';
 import CounterItem from './CounterItem';
+import {useState} from 'react';
 
 function Counter({data}) {
+    const [activeIndexes, setActiveIndexes] = useState([]);
+
+    const reset = () => {
+        setActiveIndexes([]);
+    }
+
+    const indexCallback = (id, toggle) => {
+        if (toggle) {
+            setActiveIndexes([...activeIndexes, id]);
+        } else {
+            setActiveIndexes(activeIndexes.filter((index) => index !== id));
+        }
+    }
     return <div className="counter">
+        <div className="counter-header">
         <div className="counter-title">Row Count</div>
+        <button className="reset-button" onClick={() => reset()}>Reset</button>
+        </div>
         <div className="counter-items">
         {data.map((item) => (
-            <CounterItem id={item.id} key={item.id} />
+            <CounterItem 
+            id={item.id} 
+            key={item.id} 
+            toggle={activeIndexes.includes(item.id)} 
+            toggleCallback={indexCallback}/>
         ))}
         </div>
     </div>
--- src/CounterItem.jsx
+++ src/CounterItem.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 
-function CounterItem({id}) {
-    const [toggle, setToggle] = useState(false);
+function CounterItem({id, toggle, toggleCallback}) {
     return <div 
         className={`counter-item ${toggle ? "selected" : ""}`} 
-        onClick={() => setToggle(!toggle)}>{id}</div>
+        onClick={() => toggleCallback(id, !toggle)}>{id + 1}</div>
 }
 
 CounterItem.propTypes = {
-    id: PropTypes.number.isRequired
+    id: PropTypes.number.isRequired,
+    toggle: PropTypes.bool,
+    toggleCallback: PropTypes.func.isRequired
 }
 
 export default CounterItem;
